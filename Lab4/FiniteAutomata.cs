a4e294d878b340880ff81abfa19326b03c7e2876
Paul <Paul.Rusz96@gmail.com>
2021-11-11 19:30:23 +0200

    - Reads the elements of a FA (from file) - Displays the elements of a finite automata, using a menu: the set of states, the alphabet, all the transitions, the set of final states. - Checks if a FA is DFA

---
+++ Lab4/FiniteAutomata.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lab4
+{
+    public class FiniteAutomata
+    {
+        public List<string> Q { get; }
+        public List<string> E { get; }
+        public List<string> F { get; }
+
+        public List<Tuple<Tuple<string,string>, string>> S { get; } = new();
+
+        private string q0;
+
+        public FiniteAutomata(string faFilePath)
+        {
+            using var file = new StreamReader(faFilePath);
+
+            var lines = File.ReadAllLines(faFilePath);
+            
+            Q = new List<string>(lines[0].Split(" ") ?? Array.Empty<string>());
+            Q.Remove("Q");
+            Q.Remove("=");
+
+            E = new List<string>(lines[1].Split(" ") ?? Array.Empty<string>());
+            E.Remove("E");
+            E.Remove("=");
+
+            q0 = lines[2].Split(" ")[^1];
+
+            F = new List<string>(lines[3].Split(" ") ?? Array.Empty<string>());
+            F.Remove("F");
+            F.Remove("=");
+
+            for (var i = 5; i < lines.Length; i++)
+            {
+                var s = lines[i].Split(',', '(', ')', ' ', '-', '>', '\t');
+                
+                var elements = s.Where(item => item != "").ToList();
+
+                var key = new Tuple<string, string>(elements[0], elements[1]);
+                var value = elements[2];
+
+                S.Add(new Tuple<Tuple<string, string>, string>(key, value));
+            }
+        }
+
+        public bool CheckIfDFA()
+        {
+            foreach (var element in S)
+            {
+                var key = element.Item1;
+                
+                var found = S.Count(toCheck => Equals(toCheck.Item1, key));
+
+                if (found != 1)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,9 +1,94 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lab4
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            var fa = new FiniteAutomata("FA.in");
 
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Display states");
+                Console.WriteLine("2. Display alphabet");
+                Console.WriteLine("3. Display transitions");
+                Console.WriteLine("4. Display final states");
+                Console.WriteLine("5. Check if the sequence is accepted");
+
+                var command = Convert.ToInt32(Console.ReadLine());
+
+                switch (command)
+                {
+                    case 0:
+                        goto Exit;
+                    case 1:
+                        PrintStates(fa);
+                        break;
+                    case 2:
+                        PrintAlphabet(fa);
+                        break;
+                    case 3:
+                        PrintTransition(fa);
+                        break;
+                    case 4:
+                        PrintFinalStates(fa);
+                        break;
+                    case 5:
+                        CheckSequence(fa);
+                        break;
+                }
+            }
+
+            Exit:
+            Console.WriteLine("Exiting program");
+        }
+
+        private static void PrintStates(FiniteAutomata fa)
+        {
+            Console.WriteLine();
+            var result = fa.Q.Aggregate("States: ", (current, item) => current + item + " ");
+
+            Console.WriteLine(result);
+        }
+
+        private static void PrintAlphabet(FiniteAutomata fa)
+        {
+            Console.WriteLine();
+            var result = fa.E.Aggregate("Alphabet: ", (current, item) => current + item + " ");
+
+            Console.WriteLine(result);
+        }
+
+        private static void PrintTransition(FiniteAutomata fa)
+        {
+            Console.WriteLine();
+            var result = fa.S.Aggregate("Transitions: \n", (current, item) => current + item + " \n");
+
+            Console.WriteLine(result);
+        }
+
+        private static void PrintFinalStates(FiniteAutomata fa)
+        {
+            Console.WriteLine();
+            var result = fa.F.Aggregate("Final states: ", (current, item) => current + item + " ");
+
+            Console.WriteLine(result);
+        }
+
+        private static void CheckSequence(FiniteAutomata fa)
+        {
+            if (!fa.CheckIfDFA())
+            {
+                Console.WriteLine("Not DFA");
+                return;
+            }
+            
+            Console.WriteLine("DFA");
+            
+        }
     }
 }
